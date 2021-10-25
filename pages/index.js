cfb16bd08d0440d0278b2ad67a751edaee01029f
Miguel <miguelapb35@gmail.com>
2021-10-25 19:24:28 +0200

    whatsapp

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { createClient } from "contentful";
 import RecipeCard from "../components/RecipeCard";
+import { SiWhatsapp } from "react-icons/si";
+import Link from "next/link";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -21,6 +23,15 @@ export default function Recipes({ recipes }) {
   console.log(recipes);
   return (
     <div className="recipe-list">
+      <h1 className="whatsapp">
+        <Link
+          /* href={`https://api.whatsapp.com/send?phone=4917648737898&text=Quiero hacer una donación..`} */
+          href={`https://web.whatsapp.com/send?phone=4917648737898&text=Hola tengo una consulta Miguel, aparezco en mobil`}
+        >
+          <SiWhatsapp />
+        </Link>
+      </h1>
+
       {recipes.map((recipe) => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
       ))}
@@ -31,6 +42,11 @@ export default function Recipes({ recipes }) {
             display: grid;
             grid-template-columns: 1fr 1fr;
             grid-gap: 20px 60px;
+          }
+          .whatsapp {
+            color: green;
+            font-size: 50px;
+            cursor: pointer;
           }
         `}
       </style>
