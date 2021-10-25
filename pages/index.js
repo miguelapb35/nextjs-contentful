7082f689352f16059f957458ef65d79e80a56217
Miguel <miguelapb35@gmail.com>
2021-10-25 20:48:53 +0200

    whatsappweb

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { createClient } from "contentful";
 import RecipeCard from "../components/RecipeCard";
 import { SiWhatsapp } from "react-icons/si";
+import { GiLaptop } from "react-icons/gi";
 import Link from "next/link";
 
 export async function getStaticProps() {
@@ -23,14 +24,28 @@ export default function Recipes({ recipes }) {
   console.log(recipes);
   return (
     <div className="recipe-list">
-      <h1 className="whatsapp">
-        <Link
-          href={`https://api.whatsapp.com/send?phone=4917648737898&text=Quiero hacer una donación..`}
-          /* href={`https://web.whatsapp.com/send?phone=4917648737898&text=Hola tengo una consulta Miguel, aparezco en mobil`} */
-        >
+      <a
+        title="mobil"
+        className="whatsapp"
+        href={`https://api.whatsapp.com/send?phone=4917648737898&text=Hola, tengo una pregunta...`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="icon">
           <SiWhatsapp />
-        </Link>
-      </h1>
+        </div>
+      </a>
+      <a
+        title="WhatsApp Web"
+        className="whatsappweb"
+        href={`https://web.whatsapp.com/send?phone=4917648737898&text=Hola, tengo una pregunta...`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div className="icon">
+          <GiLaptop />
+        </div>
+      </a>
 
       {recipes.map((recipe) => (
         <RecipeCard key={recipe.sys.id} recipe={recipe} />
@@ -44,9 +59,39 @@ export default function Recipes({ recipes }) {
             grid-gap: 20px 60px;
           }
           .whatsapp {
-            color: green;
-            font-size: 50px;
             cursor: pointer;
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 40px;
+            background-color: #25d366;
+            color: #fff;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+            margin-top: 16px;
+          }
+          .whatsappweb {
+            cursor: pointer;
+            position: fixed;
+            width: 60px;
+            height: 60px;
+            bottom: 40px;
+            right: 120px;
+            background-color: #25d366;
+            color: #fff;
+            border-radius: 50px;
+            text-align: center;
+            font-size: 30px;
+            box-shadow: 2px 2px 3px #999;
+            z-index: 100;
+            margin-top: 16px;
+          }
+          .icon {
+            margin-top: 14px;
           }
         `}
       </style>
